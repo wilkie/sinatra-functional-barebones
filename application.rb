9e9a0730299644122fd4a9e104827ba480f609df
wilkie <wilkie05@gmail.com>
2012-08-23 18:00:04 -0400

    Enabled html5 for haml.

--- application.rb
+++ application.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'mongo_mapper'
 
 class Application < Sinatra::Base
+  # Use HTML5
+  set :haml, :format => :html5
 end
 
 %w(config helpers controllers models).each do |dir|
