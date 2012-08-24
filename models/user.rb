82ea1f59193922585219c7fd2276d8918e4db914
wilkie <wilkie05@gmail.com>
2012-08-24 19:37:46 -0400

    Adds password hashing to User model.

--- models/user.rb
+++ models/user.rb
@@ -10,6 +10,17 @@ class User
 
   def self.create_from_params(params)
     create!(:name => params['name'],
-            :bio  => params['bio'])
+            :bio  => params['bio'],
+            :hashed_password => User.hash_password(params['password']))
+  end
+
+  key :hashed_password, String
+
+  def self.hash_password(password)
+    BCrypt::Password.create(password, :cost => Application::BCRYPT_ROUNDS)
+  end
+
+  def authenticated?(password)
+    BCrypt::Password.new(hashed_password) == password
   end
 end
