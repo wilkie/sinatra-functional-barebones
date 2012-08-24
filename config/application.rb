ca7b8b93ac44f793483727613f1325427d00d0d9
wilkie <wilkie05@gmail.com>
2012-08-24 19:40:10 -0400

    Require login.

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ class Application < Sinatra::Base
   # Application specific flags and such go here
   
   # Whether or not there is a login functionality
-  REQUIRE_LOGIN = false
+  REQUIRE_LOGIN = true
 
   # %%DELETE%% omniauth
 
