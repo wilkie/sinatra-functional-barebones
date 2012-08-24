4ca0c34ef5bf3c1ef65b3b4784d91e678cc7ebea
wilkie <wilkie05@gmail.com>
2012-08-24 15:02:43 -0400

    Adds edit action for User in users controller.

--- controllers/user.rb
+++ controllers/user.rb
@@ -1,5 +1,6 @@
 class Application
   get '/users' do
+    current_user
     @users = User.all
     haml :"users/index"
   end
@@ -11,6 +12,16 @@ class Application
 
   get '/users/new' do
     haml :"users/new"
+  end
+
+  get '/users/:id/edit' do
+    @user = User.first(:id => params[:id])
+    haml :"users/edit"
+  end
+
+  post '/users/:id/edit' do
+    user = User.first(:id => params[:id])
+    user.update_attributes!(params)
   end
 
   get '/users/:id' do
