4aa3c8a9c809a8680e653014529e247e6b635181
wilkie <wilkie05@gmail.com>
2012-08-24 23:34:04 -0400

    adds twitter signup/login logic to auth callback.

--- controllers/authorization.rb
+++ controllers/authorization.rb
@@ -9,6 +9,21 @@ class Application
       twitter = TwitterInfo.create_with_omniauth(auth)
     end
 
+    unless twitter.user
+      if Application::ALLOW_TWITTER_SIGNUP
+        twitter.user = User.create(:name => twitter.nickname,
+                                   :bio  => twitter.description,
+                                   :twitter_info => twitter)
+        twitter.save
+      end
+    end
+
+    unless logged_in?
+      session[:user_id]    = twitter.user.id
+      session[:user_token] = auth['credentials']['token']
+      session[:provider]   = :twitter
+    end
+
     session[:twitter_secret] = auth['credentials']['secret']
     session[:twitter_token]  = auth['credentials']['token']
 
