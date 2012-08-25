d6db62601485b08e220b81f74e4d6c4bb1084bb5
wilkie <wilkie05@gmail.com>
2012-08-25 14:13:52 -0400

    Do not add providers if they are not used to login.

--- controllers/session.rb
+++ controllers/session.rb
@@ -2,7 +2,9 @@ class Application
   get '/login' do
     @providers = []
     ENV['authorization_providers'].split.each do |provider|
-      @providers << {:url => "/auth/#{provider}", :name => provider}
+      if Application.constants.include?(:"ALLOW_#{provider.upcase}_SIGNUP")
+        @providers << {:url => "/auth/#{provider}", :name => provider}
+      end
     end
     haml :"session/login"
   end
