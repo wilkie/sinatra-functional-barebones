13c614248560652c03191e016f1218eedddd406e
wilkie <wilkie05@gmail.com>
2012-08-24 23:00:10 -0400

    Adds authorization initalization of omniauth.

--- config/authorization.rb
+++ config/authorization.rb
@@ -1,3 +1,15 @@
 class Application < Sinatra::Base
   use Rack::Session::Cookie
+
+  use OmniAuth::Builder do
+    providers = ENV['authorization_providers']
+    if providers
+      ENV['authorization_providers'].split.each do |service|
+        provider service.to_sym,
+          ENV["authorization_#{service}_id"],
+          ENV["authorization_#{service}_secret"],
+          :callback_path => "/auth/#{service}/callback"
+      end
+    end
+  end
 end
