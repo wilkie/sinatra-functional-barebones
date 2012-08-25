f9ad599286ccd9f7fe1ca24874e9a23e61984246
wilkie <wilkie05@gmail.com>
2012-08-25 01:28:07 -0400

    Adds github auth callback to authorizations controller.

--- controllers/authorization.rb
+++ controllers/authorization.rb
@@ -1,8 +1,21 @@
 class Application
   # %%DELETE%% twitter
 
-  # %%DELETE%% github
+  get '/auth/github/callback' do
+    auth = request.env['omniauth.auth']
 
+    github = GithubInfo.first(:uid => auth['uid'].to_i)
+    if github
+      github.update_with_omniauth(auth)
+    else
+      github = GithubInfo.create_with_omniauth(auth)
+    end
+
+    session[:github_token] = auth['credentials']['token']
+
+    redirect '/'
+  end
+ 
   # %%DELETE%% facebook
 
   # %%DELETE%% steam
