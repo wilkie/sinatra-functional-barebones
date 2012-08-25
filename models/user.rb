a8fc44bf4c40a41b0942bab28f3f8a871d33f222
wilkie <wilkie05@gmail.com>
2012-08-25 01:29:09 -0400

    Adds authorization info association placeholders in User model.

--- models/user.rb
+++ models/user.rb
@@ -8,6 +8,14 @@ class User
 
   timestamps!
 
+  # %%DELETE%% twitter
+
+  # %%DELETE%% github
+
+  # %%DELETE%% facebook
+
+  # %%DELETE%% steam
+
   def self.create_from_params(params)
     create!(:name => params['name'],
             :bio  => params['bio'])
